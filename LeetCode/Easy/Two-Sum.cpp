062400e0b7992ae208e8abf09294993d3683045a
sjhoon10 <junghoon.shin10@gmail.com>
2025-01-12 23:28:33 +0900

    Update Two-Sum.cpp

--- LeetCode/Easy/Two-Sum.cpp
+++ LeetCode/Easy/Two-Sum.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> mp;
-
         for(int i=0; nums.size(); i++)
         {
             int n = target - nums[i];
